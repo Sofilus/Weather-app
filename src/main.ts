93a3a9d6f4c9b2f5cacebfcf93e290076ed94ecb
Sofilus <sofia00olsson@gmail.com>
2022-12-06 16:17:37 +0100

    fixed ts error msg

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,16 @@ import './style/style.scss';
 import getDataLastHour from './apiSmhi';
 
 // Hämtad data senaste från timmen som skrivs ut på sidan
-async function dataLastHourGbg() {
-
+async function dataLastHourGbg(): Promise<void> {
   // väntar på all data hämtas från APIn innan den skriver ut datan på sidan
-  const data = await getDataLastHour();
+  const data: object = await getDataLastHour() as object;
 
   // Skriver ut orten
-  const locality = document.querySelector('#locality');
+  const locality: HTMLElement = document.querySelector('#locality') as HTMLElement;
   locality.innerHTML = `<span>${data.station.name}</span>`;
- 
+
   // Skriver ut temperaturen just nu i Göteborg Landvetter
-  const temperatureNow = document.querySelector('#temperatureNow');
+  const temperatureNow: HTMLElement = document.querySelector('#temperatureNow') as HTMLElement;
   temperatureNow.innerHTML = `<span>${data.value[0].value}</span>`;
   console.log(data);
 }
