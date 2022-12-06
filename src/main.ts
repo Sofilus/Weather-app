bff97674a773a1aea08f729c1028b0a2d392e3eb
Sofilus <sofia00olsson@gmail.com>
2022-12-06 14:58:00 +0100

    Temprature showing on screen Gothenburg

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,24 @@ import './style/style.scss';
 
 import getDataLastHour from './apiSmhi';
 
-async function whaitForData() {
+// Hämtar datan från vår API och skriver ut grader på sidan
+async function waitForData() {
+
+  // väntar på att getDataLastHour är klar innan den kör funktionen
   const data = await getDataLastHour();
+ 
+  // Skriver ut temperaturen just nu i Göteborg Landvetter
+  const temperatureNow = document.querySelector('#temperatureNow');
+  temperatureNow.innerHTML = `<span>${data.value[0].value}</span>`;
   console.log(data);
 }
 
-whaitForData();
+waitForData();
 
 /**
  * TODO
  x Hämta api från SMHI med info om tempratur senaste timmen
- * Få tempraturen att visas på skärmen
+ x Få tempraturen att visas på skärmen
  * Få orten att vara samma som den från vår API
  * Ändra utseende på stylingen beroende på årstid
  */
