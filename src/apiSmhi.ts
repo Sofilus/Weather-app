f085018a1e9f14a7dff41238c2d0c21ea4fd76db
Sofilus <sofia00olsson@gmail.com>
2022-12-06 01:53:55 +0100

    retrived data from API smhi

---
+++ src/apiSmhi.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line max-len
+const url = 'https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station/72420/period/latest-hour/data.json';
+
+async function getDataLastHour() {
+  return fetch(url)
+    .then((data) => {
+      console.log(data);
+      return data.json();
+    })
+    .then((json) => {
+      console.log('Aktuell temperatur:', json.value[0].value, 'grader');
+      console.log('Resterande data:');
+      return json;
+    })
+    .catch((err) => {
+      console.error(err);
+      return null;
+    });
+}
+
+const tempratureLastHourGbg = await getDataLastHour();
+
+export default tempratureLastHourGbg;
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 import './style/style.scss';
+
+import tempratureLastHourGbg from './apiSmhi';
+
+console.log(tempratureLastHourGbg);
+
+/**
+ * TODO
+ * Hämta api från SMHI med info om tempratur senaste timmen
+ * Få tempraturen att visas på skärmen
+ * Få orten att vara samma som den från vår API
+ * Ändra utseende på stylingen beroende på årstid
+ */
