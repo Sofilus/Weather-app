53ad3a250a0dd409a50e84e0e4f0e2aa1fc51988
Sofilus <sofia00olsson@gmail.com>
2022-12-06 17:52:35 +0100

    changes background img depending on season

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import './style/style.scss';
 
 import getDataLastHour from './apiSmhi';
 
+/**
+ * Data från API som skrivs ut och hämtas
+ */
+
 // Hämtad data senaste från timmen som skrivs ut på sidan
 async function dataLastHourGbg(): Promise<void> {
   // väntar på all data hämtas från APIn innan den skriver ut datan på sidan
@@ -20,9 +24,27 @@ async function dataLastHourGbg(): Promise<void> {
 await dataLastHourGbg();
 
 /**
+ * Bakgrundsbild ändras beroende på årstid
+ */
+const backgroundImg: HTMLElement = document.querySelector('main') as HTMLElement;
+const today = new Date();
+
+if (today.getMonth() === 11 || today.getMonth() <= 1) {
+  backgroundImg.classList.add('winter-img');
+} else if (today.getMonth() === 2 || today.getMonth() <= 4) {
+  backgroundImg.classList.add('spring-img');
+} else if (today.getMonth() === 5 || today.getMonth() <= 7) {
+  backgroundImg.classList.add('summer-img');
+} else {
+  backgroundImg.classList.add('fall-img');
+}
+
+/**
  * TODO
  x Hämta api från SMHI med info om tempratur senaste timmen
  x Få tempraturen att visas på skärmen
  x Få orten att vara samma som den från vår API
  * Ändra utseende på stylingen beroende på årstid
+ *[] beroende på månad ändra bakgrundsbild
+ *
  */
