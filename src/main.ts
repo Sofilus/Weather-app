9698eb341e13dfb3bda47804c3974934b4a0bedd
Sofilus <sofia00olsson@gmail.com>
2022-12-07 14:34:29 +0100

    searchfield div disappears and appears focused

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,40 @@ import getDataLastHour from './api-smhi-temp-last-hour';
 import getDataWind from './api-smhi-wind';
 
 /**
- * Data från API som skrivs ut och hämtas
+ * Få reda på användarens position
+ */
+const successCallback = (position: object) => {
+  console.log(position);
+};
+const errorCallback = (error) => {
+  console.log(error);
+};
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+/**
+ * Dropdown på sök ort inputen
+ */
+
+// Kallar på alla viktiga element i sök ort
+const searchField: HTMLElement = document.querySelector('#searchField') as HTMLElement;
+const searchDropdown: HTMLElement = document.querySelector('#searchDropdown') as HTMLElement;
+
+// Skapar eventlisteners
+searchField?.addEventListener('focus', openDropdown);
+searchField?.addEventListener('blur', closeDropdown);
+
+// Öppnar dropdown
+function openDropdown(): void {
+  searchDropdown.classList.remove('display-none');
+}
+
+// Stänger dropdown
+function closeDropdown(): void {
+  searchDropdown.classList.add('display-none');
+}
+
+/**
+ * Data från API som skrivs ut och används på sidan
  */
 
 // Hämtad data med tempratur från senaste timmen som skrivs ut på sidan
@@ -25,6 +58,7 @@ async function dataLastHourGbg(): Promise<void> {
 
 await dataLastHourGbg();
 
+// Hämtad data med vindhastighet som skrivs ut på sidan
 async function dataWind(): Promise<void> {
   // väntar på all data hämtas från APIn innan den skriver ut datan på sidan
   const data: object = await getDataWind() as object;
@@ -36,17 +70,6 @@ async function dataWind(): Promise<void> {
 }
 
 await dataWind();
-
-/**
- * Få reda på användarens position
- */
-const successCallback = (position: object) => {
-  console.log(position);
-};
-const errorCallback = (error) => {
-  console.log(error);
-};
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
 /**
  * Bakgrundsbild ändras beroende på årstid
@@ -66,6 +89,9 @@ if (today.getMonth() === 11 || today.getMonth() <= 1) {
 
 /**
  * TODO
- * [] Visa nederbörd
- * 
+ * [x] När jag klickar på inputrutan ska ett fält komma upp med en ul och li med min plats
+ * [] När fältet inte är markerat ska klassen förvinna
+ * [] När jag klickar på min position ska webblöäsaren fråga efter användarens plats
+ * [] Koordinaterna ska skrivas ut som en plats
+ * [] Min position ska skrivas ut i rubriken ort
  */
