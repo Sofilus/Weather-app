b9c5b666a855087043f2ca6763a67fabd918d9cf
Sofilus <sofia00olsson@gmail.com>
2023-03-08 17:16:09 +0100

    updated url to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/fed22d-js-grundkurs-2-weather-app-Sofilus/',
+  base: '/Weather-app/',
   define: {
     'process.env': {},
   },
